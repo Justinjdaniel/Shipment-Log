a0f84236b54e91d931c12d87bc6ada10080fbd98
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2022-01-01 13:44:27 +0530

    routes ts

---
+++ server/ts/server/src/routes/profile.ts
@@ -0,0 +1,16 @@
+const profile = {
+  shipper: {
+    Wallet: `<path>/network/wallets/Shipper`,
+    CCP: `<path>/network/gateways/Shipper/Shipper gateway.json`,
+  },
+  auth1: {
+    Wallet: `<path>/network/wallets/Auth1`,
+    CCP: `<path>/network/gateways/Auth1/Auth1 gateway.json`,
+  },
+  auth2: {
+    Wallet: `<path>/network/wallets/Auth2`,
+    CCP: `<path>/network/gateways/Auth2/Auth2 gateway.json`,
+  },
+};
+
+export default  profile ;
