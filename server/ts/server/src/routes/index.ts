ef30d561b67efeb089a4163468d3f3aab24f3a07
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2022-02-05 11:11:37 +0530

    TS Server Version removed

--- server/ts/server/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-
-const baseRouter = Router();
-baseRouter.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-export default baseRouter;
